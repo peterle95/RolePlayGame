270b41492a6b0d84c493a37a45a9ce2ff1bd20fb
Peter Moelzer <moelzerpeter@gmail.com>
2025-01-13 18:48:19 +0100

    comment

--- ex00/rip/rip.c
+++ ex00/rip/rip.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
 
-int is_balanced(char *str) {
+int is_balanced(char *str) 
+{
     int count = 0;
-    for (int i = 0; str[i]; i++) {
+    for (int i = 0; str[i]; i++) 
+    {
         if (str[i] == '(')
             count++;
         else if (str[i] == ')')
@@ -13,37 +15,45 @@ int is_balanced(char *str) {
     return count == 0;
 }
 
-void print_solution(char *str) {
+void print_solution(char *str) 
+{
     int i = 0;
-    while (str[i]) {
+    while (str[i]) 
+    {
         write(1, &str[i], 1);
         i++;
     }
     write(1, " \n", 2);
 }
 
-void remove_parentheses(char *str, int pos, char *current, int removed) {
-    if (str[pos] == '\0') {
+void remove_parentheses(char *str, int pos, char *current, int removed) 
+{
+    if (str[pos] == '\0') 
+    {
         if (is_balanced(current))
             print_solution(current);
         return;
     }
     current[pos] = str[pos];
     remove_parentheses(str, pos + 1, current, removed);
-    if (removed > 0 && (str[pos] == '(' || str[pos] == ')')) {
+    if (removed > 0 && (str[pos] == '(' || str[pos] == ')')) 
+    {
         current[pos] = ' ';
         remove_parentheses(str, pos + 1, current, removed - 1);
     }
 }
 
-int count_unbalanced(char *str) {
+int count_unbalanced(char *str) 
+{
     int open = 0;
     int unbalanced = 0;
     
-    for (int i = 0; str[i]; i++) {
+    for (int i = 0; str[i]; i++) 
+    {
         if (str[i] == '(')
             open++;
-        else if (str[i] == ')') {
+        else if (str[i] == ')') 
+        {
             if (open == 0)
                 unbalanced++;
             else
@@ -53,7 +63,8 @@ int count_unbalanced(char *str) {
     return unbalanced + open;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     if (argc != 2) {
         write(1, "\n", 1);
         return 1;
@@ -63,8 +74,10 @@ int main(int argc, char **argv) {
     int len = 0;
     int has_open = 0;
     int has_close = 0;
-    while (input[len]) {
-        if (input[len] != '(' && input[len] != ')') {
+    while (input[len]) 
+    {
+        if (input[len] != '(' && input[len] != ')') 
+        {
             write(1, "\n", 1);
             return 1;
         }
@@ -74,7 +87,8 @@ int main(int argc, char **argv) {
             has_close = 1;
         len++;
     }
-    if (!has_open || !has_close) {
+    if (!has_open || !has_close) 
+    {
         write(1, "\n", 1);
         return 1;
     }
@@ -82,7 +96,8 @@ int main(int argc, char **argv) {
     for (int i = 0; i <= len; i++)
         current[i] = input[i];
     int min_remove = count_unbalanced(input);
-    if (min_remove == 0) {
+    if (min_remove == 0) 
+    {
         print_solution(input);
         return 0;
     }
