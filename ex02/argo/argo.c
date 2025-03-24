29bf405125c923c4b043d538eee3a89e40078486
Peter Moelzer <moelzerpeter@gmail.com>
2025-03-24 14:52:30 +0100

    norm

--- ex02/argo/argo.c
+++ ex02/argo/argo.c
@@ -1,22 +1,36 @@
 #include "argo.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int	peek(FILE *stream)
+int peek(FILE *stream)
 {
-	int	c = getc(stream);
-	ungetc(c, stream);
+	if (!stream)
+		return EOF;
+	int c = getc(stream);
+	if (c != EOF)
+		ungetc(c, stream);
 	return c;
 }
 
-void	unexpected(FILE *stream)
+void unexpected(FILE *stream)
 {
-	if (peek(stream) != EOF)
-		printf("unexpected token '%c'\n", peek(stream));
+	if (!stream)
+	{
+		printf("unexpected null stream\n");
+		return;
+	}
+	int c = peek(stream);
+	if (c != EOF)
+		printf("unexpected token '%c'\n", c);
 	else
 		printf("unexpected end of input\n");
 }
 
-int	accept(FILE *stream, char c)
+int accept(FILE *stream, char c)
 {
+	if (!stream)
+		return 0;
 	if (peek(stream) == c)
 	{
 		(void)getc(stream);
@@ -25,16 +39,23 @@ int	accept(FILE *stream, char c)
 	return 0;
 }
 
-int	expect(FILE *stream, char c)
+int expect(FILE *stream, char c)
 {
+	if (!stream)
+	{
+		unexpected(stream);
+		return 0;
+	}
 	if (accept(stream, c))
 		return 1;
 	unexpected(stream);
 	return 0;
 }
 
-int	parse_int(json *dst, FILE *stream)
+int parse_int(json *dst, FILE *stream)
 {
+	if (!dst || !stream)
+		return -1;
 	int n = 0;
 	int c = peek(stream);
 	
@@ -44,7 +65,7 @@ int	parse_int(json *dst, FILE *stream)
 	}
 	
 	while (isdigit(c)) {
-		getc(stream); // consume digit
+		getc(stream);
 		n = n * 10 + (c - '0');
 		c = peek(stream);
 	}
@@ -56,12 +77,15 @@ int	parse_int(json *dst, FILE *stream)
 
 char *get_str(FILE *stream)
 {
+	if (!stream)
+		return NULL;
 	size_t capacity = 16;
 	size_t i = 0;
 	char *res = malloc(capacity);
-	if (!res) return NULL;
-	
-	int c = getc(stream); // Consume opening quote
+	if (!res) 
+		return NULL;
+
+	int c = getc(stream);
 	if (c != '"') {
 		free(res);
 		return NULL;
@@ -69,29 +93,33 @@ char *get_str(FILE *stream)
 
 	while (1) {
 		c = getc(stream);
-		if (c == '"') break;
+		if (c == '"')
+			break;
 		if (c == EOF) {
 			free(res);
 			unexpected(stream);
 			return NULL;
 		}
-		if (c == '\\') {
+		if (c == '\\') 
+		{
 			c = getc(stream);
-			switch (c) {
-				case '\\': c = '\\'; break;
-				case '"':  c = '"';  break;
-				case '/':  c = '/';  break;
-				case 'b':  c = '\b'; break;
-				case 'f':  c = '\f'; break;
-				case 'n':  c = '\n'; break;
-				case 'r':  c = '\r'; break;
-				case 't':  c = '\t'; break;
-				default:
-					free(res);
-					return NULL; // Invalid escape
+			if (c == EOF) 
+			{
+				free(res);
+				unexpected(stream);
+				return NULL;
+			}
+			if (c == '\\')
+				c = '\\';
+			else if (c == '"')
+				c = '"';
+			else {
+				free(res);
+				return NULL;
 			}
 		}
-		if (i >= capacity-1) {
+		if (i >= capacity - 1) 
+		{
 			capacity *= 2;
 			char *new_res = realloc(res, capacity);
 			if (!new_res) {
@@ -108,31 +136,40 @@ char *get_str(FILE *stream)
 
 int parse_map(json *dst, FILE *stream)
 {
+	if (!dst || !stream)
+		return -1;
 	dst->type = MAP;
 	dst->map.size = 0;
 	dst->map.data = NULL;
 	
-	if (!expect(stream, '{')) return -1;
+	if (!expect(stream, '{'))
+		return -1;
 
-	if (accept(stream, '}')) {
+	if (accept(stream, '}'))
 		return 1;
-	}
 
 	while (1)
 	{
-		if (peek(stream) != '"') {
+		if (peek(stream) != '"')
+		{
 			unexpected(stream);
 			return -1;
 		}
-		char c;
-		pair *new_data = realloc(dst->map.data, (dst->map.size+1)*sizeof(pair));
-		if (!new_data) {
+		int c;
+		pair *new_data = realloc(dst->map.data, (dst->map.size + 1) * sizeof(pair));
+		if (!new_data)
+		{
 			free(dst->map.data);
 			return -1;
 		}
 		dst->map.data = new_data;
+		
 		pair *current = &dst->map.data[dst->map.size];
-		memset(current, 0, sizeof(pair));
+
+		current->key = NULL;
+		current->value.type = INTEGER;
+		current->value.integer = 0;
+
 		current->key = get_str(stream);
 		if (current->key == NULL)
 			return -1;
@@ -144,8 +181,8 @@ int parse_map(json *dst, FILE *stream)
 		c = peek(stream);
 		if (c == '}')
 		{
-			accept(stream ,c);
-			break ;
+			accept(stream, c);
+			break;
 		}
 		if (c == ',')
 			accept(stream, ',');
@@ -160,8 +197,10 @@ int parse_map(json *dst, FILE *stream)
 
 int parser(json *dst, FILE *stream)
 {
-	int c = peek(stream);
+	if (!dst || !stream)
+		return -1;
 
+	int c = peek(stream);
 	if (c == EOF)
 	{
 		unexpected(stream);
@@ -185,7 +224,6 @@ int parser(json *dst, FILE *stream)
 	return (1);
 }
 
-//free_json() added in the main
 int argo(json *dst, FILE *stream)
 {
 	if (!stream || !dst) {
